a1258081fc05369567949b1a42c0ef20ac84949e
Gregory Evans <evansdsg2@MacBook-Pro.local>
2018-10-30 10:51:26 -0400

    Chapter II.

--- code examples/AttachmentHandler.py
+++ code examples/AttachmentHandler.py
@@ -6,8 +6,10 @@ class AttachmentHandler:
         self,
         starting_dynamic=None,
         ending_dynamic=None,
-        hairpin_indicator=None,
-        articulation=None,
+        hairpin=None,
+        articulation_list=None,
+        text_list=None,
+        line_style=None,
         ):
         def cyc(lst):
             count = 0
@@ -16,12 +18,58 @@ class AttachmentHandler:
                 count += 1
         self.starting_dynamic = starting_dynamic
         self.ending_dynamic = ending_dynamic
-        self.hairpin_indicator = hairpin_indicator
-        self.articulation = articulation
+        self.hairpin = hairpin
+        self.articulation_list = articulation_list
+        self.text_list = text_list
+        self.line_style = line_style
+        self._cyc_articulations = cyc(articulation_list)
         self._cyc_dynamics = cyc([starting_dynamic, ending_dynamic])
+        self._cyc_text = cyc(text_list)
 
     def __call__(self, selections):
         return self.add_attachments(selections)
+
+    def _apply_text_and_span_lr(self, selections):
+        text = self._cyc_text
+        for run in abjad.select(selections).runs():
+            leaves = abjad.select(run).leaves()
+            span = abjad.StartTextSpan(
+                command=r'\startTextSpanOne',
+                left_text=abjad.Markup(next(text)).upright(),
+                right_text=abjad.Markup(next(text)).upright(),
+                style=self.line_style,
+                )
+            abjad.attach(span, leaves[0])
+            abjad.attach(abjad.StopTextSpan(command=r'\stopTextSpanOne',), leaves[-1])
+
+    def _apply_text_and_span_l_long(self, selections):
+        text = self._cyc_text
+        for run in abjad.select(selections).runs():
+            leaves = abjad.select(run).leaves()
+            span = abjad.StartTextSpan(
+                command=r'\startTextSpanOne',
+                right_padding=2.5,
+                left_text=abjad.Markup(next(text)).upright(),
+                style='solid-line-with-hook',
+                )
+            last_leaf = leaves[-1]
+            next_leaf = abjad.inspect(last_leaf).leaf(1)
+            abjad.attach(span, leaves[0])
+            abjad.attach(abjad.StopTextSpan(command=r'\stopTextSpanOne',), leaves[-1])
+
+    def _apply_text_and_span_l_short(self, selections):
+        text = self._cyc_text
+        for run in abjad.select(selections).runs():
+            leaves = abjad.select(run).leaves()
+            span = abjad.StartTextSpan(
+                command=r'\startTextSpanOne',
+                right_padding=2.5,
+                left_text=abjad.Markup(next(text)).upright(),
+                style='solid-line-with-hook',
+                )
+            last_leaf = leaves[-1]
+            next_leaf = abjad.inspect(last_leaf).leaf(1)
+            abjad.attach(span, leaves[0])
 
     def add_attachments(self, selections):
         runs = abjad.select(selections).runs()
@@ -29,16 +77,35 @@ class AttachmentHandler:
         for run in runs:
             if len(run) > 1:
                 leaves = abjad.select(run).leaves()
-                abjad.attach(abjad.Dynamic(self.starting_dynamic), leaves[0])
-                abjad.attach(abjad.HairpinIndicator(self.hairpin_indicator), leaves[0])
-                abjad.attach(abjad.Dynamic(self.ending_dynamic), leaves[-1])
-                abjad.attach(abjad.HairpinIndicator('--'), leaves[-1])
+                if self.starting_dynamic != None:
+                    abjad.attach(abjad.Dynamic(self.starting_dynamic), leaves[0])
+                if self.hairpin != None:
+                    abjad.attach(abjad.StartHairpin(self.hairpin), leaves[0])
+                if self.ending_dynamic != None:
+                    abjad.attach(abjad.Dynamic(self.ending_dynamic), leaves[-1])
+                abjad.attach(abjad.StartHairpin('--'), leaves[-1])
+                if self.text_list != None:
+                    if len(self.text_list) > 1:
+                        self._apply_text_and_span_lr(run)
+                    else:
+                        self._apply_text_and_span_l_long(run)
             else:
                 leaves = abjad.select(run).leaves()
                 dynamic = next(self._cyc_dynamics)
-                abjad.attach(abjad.Dynamic(dynamic), leaves[0])
-                abjad.attach(abjad.HairpinIndicator('--'), leaves[0])
+                if self.starting_dynamic != None:
+                    if self.ending_dynamic != None:
+                        abjad.attach(abjad.Dynamic(dynamic), leaves[0])
+                    else:
+                        abjad.attach(abjad.Dynamic(self.starting_dynamic), leaves[0])
+                if self.starting_dynamic == None:
+                    if self.ending_dynamic != None:
+                        abjad.attach(abjad.Dynamic(self.ending_dynamic), leaves[0])
+                abjad.attach(abjad.StartHairpin('--'), leaves[0])
+                if self.text_list != None:
+                    self._apply_text_and_span_l_short(run)
         for tie in ties:
             if len(tie) == 1:
-                abjad.attach(abjad.Articulation(self.articulation), tie[0])
+                if self.articulation_list != None:
+                    articulation = self._cyc_articulations
+                    abjad.attach(abjad.Articulation(next(articulation)), tie[0])
         return selections
--- code examples/Example_Code.py
+++ code examples/Example_Code.py
@@ -23,7 +23,7 @@ import abjad
 # notes = [abjad.Note(x, y) for x, y in zip(pitches, durations)]
 # note_staff = abjad.Staff(notes)
 # abjad.show(note_staff)
-
+#
 # ###BOW STAFF OPTIONS###
 # #Slow bow_staff
 # bow_staff = abjad.Staff()
@@ -38,20 +38,20 @@ import abjad
 # abjad.attach(indicator_4, bow_staff[3])
 # abjad.bow_contact_spanner(bow_staff, omit_bow_changes=True)
 # abjad.show(bow_staff)
-
-#Faster bow_staff
-new_bow_staff = abjad.Staff()
-new_bow_staff.extend(r"c'4 c'4 c'4 c'4")
-indicator_1 = abjad.BowContactPoint((3, 3))
-indicator_2 = abjad.BowContactPoint((2, 3))
-indicator_3 = abjad.BowContactPoint((1, 3))
-indicator_4 = abjad.BowContactPoint((0, 3))
-indicators = [indicator_1, indicator_2, indicator_3, indicator_4, ]
-leaves = abjad.select(new_bow_staff).leaves()
-for leaf, indicator in zip(leaves, indicators):
-    abjad.attach(indicator, leaf)
-abjad.bow_contact_spanner(new_bow_staff, omit_bow_changes=True)
-abjad.show(new_bow_staff)
+#
+# #Faster bow_staff
+# new_bow_staff = abjad.Staff()
+# new_bow_staff.extend(r"c'4 c'4 c'4 c'4")
+# indicator_1 = abjad.BowContactPoint((3, 3))
+# indicator_2 = abjad.BowContactPoint((2, 3))
+# indicator_3 = abjad.BowContactPoint((1, 3))
+# indicator_4 = abjad.BowContactPoint((0, 3))
+# indicators = [indicator_1, indicator_2, indicator_3, indicator_4, ]
+# leaves = abjad.select(new_bow_staff).leaves()
+# for leaf, indicator in zip(leaves, indicators):
+#     abjad.attach(indicator, leaf)
+# abjad.bow_contact_spanner(new_bow_staff, omit_bow_changes=True)
+# abjad.show(new_bow_staff)
 #
 # #Alternative Faster bow_staff
 # new_bow_staff = abjad.Staff()
@@ -74,23 +74,40 @@ abjad.show(new_bow_staff)
 # abjad.bow_contact_spanner(newer_bow_staff, omit_bow_changes=True)
 # abjad.show(newer_bow_staff)
 #
-# ###DYNAMIC ATTACHMENT###
-# #Slow dynamics
-# dynamic_staff = abjad.Staff()
-# dynamic_staff.extend(r"c'4 cs'4 d'2")
-# piano = abjad.Dynamic('p')
-# mezzo_forte = abjad.Dynamic('mf')
-# forte = abjad.Dynamic('f')
-# abjad.attach(piano, dynamic_staff[0])
-# abjad.attach(mezzo_forte, dynamic_staff[1])
-# abjad.attach(forte, dynamic_staff[2])
-# abjad.show(dynamic_staff)
-#
-# #Faster dynamics
-# new_staff = abjad.Staff()
-# new_staff.extend(r"c'4 cs'4 d'2")
-# dynamics = ['p', 'mf', 'f', ]
-# leaves = abjad.select(new_staff).leaves()
-# for leaf, dynamic in zip(leaves, dynamics):
-#     abjad.attach(abjad.Dynamic(dynamic), leaf)
-# abjad.show(new_staff)
+###DYNAMIC ATTACHMENT###
+#Slow dynamics
+dynamic_staff = abjad.Staff()
+dynamic_staff.extend(r"c'4 cs'4 d'2")
+piano = abjad.Dynamic('p')
+mezzo_forte = abjad.Dynamic('mf')
+forte = abjad.Dynamic('f')
+abjad.attach(piano, dynamic_staff[0])
+abjad.attach(mezzo_forte, dynamic_staff[1])
+abjad.attach(forte, dynamic_staff[2])
+abjad.show(dynamic_staff)
+
+#Faster dynamics
+new_staff = abjad.Staff()
+new_staff.extend(r"c'4 cs'4 d'2")
+dynamics = ['p', 'mf', 'f', ]
+leaves = abjad.select(new_staff).leaves()
+for leaf, dynamic in zip(leaves, dynamics):
+    abjad.attach(abjad.Dynamic(dynamic), leaf)
+abjad.show(new_staff)
+
+#Behavioral Dynamics
+music_staff = abjad.Staff()
+music_staff.extend(r"c'4 cs'4 d'2 r4 ds'2 e'8 f'8 fs'8 g'8 gs''8 r4 a''2.")
+for run in abjad.select(music_staff).runs():
+    if len(run) > 3:
+        leaves = abjad.select(run).leaves()
+        abjad.attach(abjad.Dynamic('mf'), run[0])
+        for leaf in leaves:
+            abjad.attach(abjad.Articulation('tenuto'), leaf)
+    elif len(run) == 3:
+        abjad.attach(abjad.Dynamic('f'), run[0])
+        abjad.attach(abjad.StartHairpin('>'), run[0])
+        abjad.attach(abjad.Dynamic('mp'), run[-1])
+    elif len(run) == 1:
+        abjad.attach(abjad.Dynamic('ppp'), run[0])
+abjad.show(music_staff)
